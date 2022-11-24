67194df7b3a6ca7920ad8c9bf28e447158f4dfed
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-11-24 17:34:50 +0100

    [add] menu js

---
+++ src/data/menu.js
@@ -0,0 +1,151 @@
+/* All menus */
+
+export default {
+
+  header: [
+    {
+      text: 'Home',
+      href: '#',
+      isActive: false
+    },
+    {
+      text: 'About',
+      href: '#',
+      isActive: false
+    },
+    {
+      text: 'Services',
+      href: '#',
+      isActive: false
+    },
+    {
+      text: 'Process',
+      href: '#',
+      isActive: false
+    },
+    {
+      text: 'Team',
+      href: '#',
+      isActive: false
+    },
+    {
+      text: 'Blog',
+      href: '#',
+      isActive: false
+    }
+  ],
+
+  contacts: {
+    text: 'A functional HTML Template for Corporate & Business',
+    menu: [
+      {
+        name: 'phone',
+        text: '+1(305)1234-5678'
+      },
+      {
+        name: 'envelope',
+        text: 'hello@example.com'
+      },
+      {
+        name: 'location-dot',
+        text: 'Main Avenue, 987'
+      }
+    ]
+  },
+
+  footer: {
+
+    about: {
+      title: 'About',
+      menu: [
+        {
+          text: 'The company',
+          href: '#'
+        },
+        {
+          text: 'Istitutional',
+          href: '#'
+        },
+        {
+          text: 'Social & Events',
+          href: '#'
+        },
+        {
+          text: 'Innovation',
+          href: '#'
+        },
+        {
+          text: 'Environment',
+          href: '#'
+        },
+        {
+          text: 'Technology',
+          href: '#'
+        }
+      ]
+    },
+
+    services: {
+      title: 'Services',
+      menu: [
+        {
+          text: 'Audit e Assurance',
+          href: '#'
+        },
+        {
+          text: 'Financial Advisory',
+          href: '#'
+        },
+        {
+          text: 'Analytics M&A',
+          href: '#'
+        },
+        {
+          text: 'Middle Marketing',
+          href: '#'
+        },
+        {
+          text: 'Legal Consulting',
+          href: '#'
+        },
+        {
+          text: 'Regulatory Risk',
+          href: '#'
+        }
+      ]
+    },
+
+    support: {
+      title: 'Support',
+      menu: [
+        {
+          text: 'Responsibility',
+          href: '#'
+        },
+        {
+          text: 'Terms of Use',
+          href: '#'
+        },
+        {
+          text: 'About Cookies',
+          href: '#'
+        },
+        {
+          text: 'Privacy Policy',
+          href: '#'
+        },
+        {
+          text: 'Accessibility',
+          href: '#'
+        },
+        {
+          text: 'Information',
+          href: '#'
+        }
+      ]
+    } 
+
+  }
+  
+}
+
